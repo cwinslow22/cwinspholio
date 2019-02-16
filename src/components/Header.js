b005b090801374b5497ce81ad5cd1ba8277915d4
Charlie Winslow <cwinslow22@yahoo.com>
2019-02-15 19:13:39 -0600

    added some images and updated favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-diamond" />
+      <span className="icon fa-share-alt" />
     </div>
     <div className="content">
       <div className="inner">
